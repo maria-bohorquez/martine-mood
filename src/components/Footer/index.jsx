e174c8e152dd5ca030d7d2121c7c9ee5290cf303
María Bohórquez <81176635+maria-bohorquez@users.noreply.github.com>
2021-09-23 12:42:33 +0200

    initial commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer, H2, H2Middle } from "./styles";
+
+const FooterComponent = () => (
+  <Footer>
+    <H2>Look at it.</H2>
+    <H2Middle> Look at my cat.</H2Middle>
+    <H2>Look at it.</H2>
+  </Footer>
+);
+
+export default FooterComponent;
